316c2e834ef29335283e4fba6204083e660c4cc1
Geon_27 <yukgeon98527@gmail.com>
2023-01-06 06:41:19 +0900

    23/01/06 FVC.swift fixed

--- SubmitValue-Back/FormViewController.swift
+++ SubmitValue-Back/FormViewController.swift
@@ -7,5 +7,24 @@
 
 import UIKit
 class FormViewController: UIViewController {
+    @IBOutlet var email: UITextField! // 이메일
+    @IBOutlet var isUpdate: UISwitch! // 자동갱신 여부
+    @IBOutlet var interval: UIStepper! // 갱신주기
     
+    //submit 버튼을 클릭했을때 호출되는 메소드
+    @IBAction func onSubmit(_ sender: AnyObject) {
+        // persentingViewController 속성을 통하여 이전화면 객체를 읽어온 후, ViewController 타입으로 캐스팅.
+        let perVC = self.presentingViewController
+        guard let vc = perVC as? ViewController else {
+            return
+        }
+        
+        // 값 전달
+        vc.paramEmail = self.email.text
+        vc.paramUpdate = self.isUpdate.isOn
+        vc.paramInterval = self.interval.value
+        
+        // 이전 화면으로 복귀
+        self.presentingViewController?.dismiss(animated: true)
+    }
 }
