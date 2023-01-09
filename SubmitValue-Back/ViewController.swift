2ad04b8a01c679a5f8f7b1880c7b9ccb6e3fc37d
Geon_27 <yukgeon98527@gmail.com>
2023-01-09 22:45:44 +0900

    23/01/09 ViewController fixed

--- SubmitValue-Back/ViewController.swift
+++ SubmitValue-Back/ViewController.swift
@@ -14,21 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet var resultInterval: UILabel!
     
   // 값을 직접 전달받을 프로퍼티들
-    var paramEmail: String? // 이메일 값을 전달받을 속성
-    var paramUpdate: Bool? // 자동 갱신 여부를 전달받을 속성
-    var paramInterval: Double? // 갱신주기 값을 전달받을 속성
+//    var paramEmail: String? // 이메일 값을 전달받을 속성
+//    var paramUpdate: Bool? // 자동 갱신 여부를 전달받을 속성
+//    var paramInterval: Double? // 갱신주기 값을 전달받을 속성
     
     // 화면이 표시 될때마다 실행되는 메소드
     override func viewWillAppear(_ animated: Bool) {
-        if let email = paramEmail {
+//        if let email = paramEmail {
+//            resultEmail.text = email
+//        }
+//        if let update = paramUpdate {
+//            resultUpdate.text = update==true ? "자동갱신":"자동갱신안함"
+//        }
+//        if let interval = paramInterval {
+//            resultInterval.text = "\(Int(interval))분마다"
+//        }
+    // AppDelegate 객체의 인스턴스를 가져온다
+        let ad = UIApplication.shared.delegate as? AppDelegate
+        
+        if let email = ad?.paramEmail {
             resultEmail.text = email
         }
-        if let update = paramUpdate {
-            resultUpdate.text = update==true ? "자동갱신":"자동갱신안함"
-        }
-        if let interval = paramInterval {
-            resultInterval.text = "\(Int(interval))분마다"
-        }
+        
     }
 }
 
