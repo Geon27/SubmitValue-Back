d44e309d57ba573291a058153dc0eab7f9e5c455
Geon_27 <yukgeon98527@gmail.com>
2023-01-10 17:40:40 +0900

    23/01/10 Viewcontroller.swift if code write

--- SubmitValue-Back/ViewController.swift
+++ SubmitValue-Back/ViewController.swift
@@ -35,7 +35,12 @@ class ViewController: UIViewController {
         if let email = ad?.paramEmail {
             resultEmail.text = email
         }
-        
+        if let update = ad?.paramUpdate {
+            resultUpdate.text = update==true ? "자동갱신":"자동갱신안함"
+        }
+        if let interval = ad?.paramInterval {
+            resultInterval.text = "\(Int(interval))분마다"
+        }
     }
 }
 
